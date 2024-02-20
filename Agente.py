a9e214bdb3126e15e8a55b672aae29dcb6ab21c5
Juan José <JuanJRiosP9@gmail.com>
2024-02-19 20:42:28 -0500

    Update Agente.py

--- Agente.py
+++ Agente.py
@@ -6,10 +6,17 @@ class Agente:
     def __init__(self):
         self.libre = True
         self.ocupacion = 0
+        self.liberar = 0
 
-    def atender(self, persona: Persona):
+    def atender(self, persona: Persona, tiempo: int) -> None:
         self.ocupacion += persona.obtener_servicio()
+        self.liberar = tiempo + persona.obtener_servicio() - 1
         self.libre = False
+        persona.atendido = True
 
-    def tiempolibre (self):
+    def liberador(self) -> None:
+        self.libre = True
+
+    def tiempolibre (self) -> int:
         return 28800 - self.ocupacion
+
