a4375bab6ab989b96e0024f1f50ecae811362049
Juan José <JuanJRiosP9@gmail.com>
2024-02-19 20:42:49 -0500

    Update Fila.py

--- Fila.py
+++ Fila.py
@@ -1,7 +1,10 @@
 class Fila:
 
-    def __init__(self, personas):
+    def __init__(self, personas: list[Persona]):
         self.personas = personas
 
     def eliminar(self):
-        self.personas.pop(0)
+        if len(self.personas) > 0:
+            self.personas.pop(0)
+        else:
+            raise IndexError("La fila esta vacia")
