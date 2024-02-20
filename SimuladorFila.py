376508442bb22522aea66c0390c591794f9fd7ba
Juan José <JuanJRiosP9@gmail.com>
2024-02-19 20:44:22 -0500

    Update SimuladorFila.py

--- SimuladorFila.py
+++ SimuladorFila.py
@@ -5,15 +5,34 @@ import Fila
 
 class SimuladorFila:
 
-    def __init__(self, agentes, personas):
+    def __init__(self, agentes: int, fila: Fila):
         self.agentes = agentes
-        self.personas = personas
+        self.personas = fila
 
     def corrersimulacion(self):
-        agentes = list()
+        agentes = []
+        tiempo_espera = []
         for i in range(self.agentes):
             agente = Agente()
             agentes.append(agente)
 
-        for i in range(0, 28800):
-            pass
+        for tiempo in range(0, 28800):
+            for persona in self.personas:
+                if persona.obtener_llegada() <= tiempo:
+                    for agente in agentes:
+                        if agente.libre == True:
+                            agente.atender(persona)
+                            tiempo_espera.append(persona.espera)
+                            self.personas.eliminar()
+                        else:
+                            if agente.liberar == tiempo:
+                                agente.liberador()
+
+                    if persona.atendido == True:
+                        pass
+                    else:
+                        persona.incrementar()
+                        break
+                else:
+                    break
+
