de6850b38848f1607ea39782021507fa1eea8da2
Juan José <JuanJRiosP9@gmail.com>
2024-02-19 20:43:05 -0500

    Update Persona.py

--- Persona.py
+++ Persona.py
@@ -9,10 +9,16 @@ class Persona:
         # Id consecutivo
         Persona.contador_id += 1
         self.__id = Persona.contador_id
-
         self.__llegada = llegada
         self.__servicio = random.randint(300, 3600)
+        self.atendido = False
         self.espera = 0
 
     def obtener_servicio(self):
         return self.__servicio
+
+    def obtener_llegada(self):
+        return self.__llegada
+
+    def incrementar(self):
+        self.espera += 1
